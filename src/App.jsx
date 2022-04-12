b037b6c48cb08f5526313f3d0d4366dfc08ee1d6
dewaleolaoye <adewaleolaoye21@gmail.com>
2022-04-12 23:59:51 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Flex } from "./shared";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Flex alignItems="center"></Flex>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/shared/Flex.styled.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const Flex = styled.div`
+  display: flex;
+  gap: ${({ gap }) => gap};
+  align-items: ${({ alignItems }) => alignItems};
+  justify-content: ${({ justifyContent }) => justifyContent};
+  flex-direction: ${({ flexDir }) => flexDir};
+  flex-wrap: ${({ flexWrap }) => flexWrap};
+  padding: ${({ padding }) => padding};
+  margin: ${({ margin }) => margin};
+`;
+
+export default Flex;
